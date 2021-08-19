91a79fdaa03c93d7082909869989f23577213567
giovannialves01 <giovanni.alves01@fatec.sp.gov.br>
2021-08-19 18:46:13 -0300

    Configuração inicial do projeto

---
+++ Projeto/src/main/java/fatec/api/Sirius/SiriusApplication.java
@@ -0,0 +1,13 @@
+package fatec.api.Sirius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiriusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiriusApplication.class, args);
+	}
+
+}
