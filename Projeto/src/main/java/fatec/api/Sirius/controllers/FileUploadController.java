a7ebae1afe53a3a069d12a873ba8ecc27faef5a1
giovannialves01 <giovanni.alves01@fatec.sp.gov.br>
2021-10-04 11:07:48 -0300

    metodo de upload funcionando sem o organizador de arquivos

--- Projeto/src/main/java/fatec/api/Sirius/controllers/FileUploadController.java
+++ Projeto/src/main/java/fatec/api/Sirius/controllers/FileUploadController.java
@@ -1,18 +1,25 @@
 package fatec.api.Sirius.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@RestController
 public class FileUploadController {
 	
 	public static String uploadDirectory = "../Root";
 	
-	@PostMapping("/UploadFiles")
-	public String upload(Model model,@RequestParam("Files") MultipartFile[] files) {	
-		System.out.println("Opa");
-		/*StringBuilder fileNames = new StringBuilder();
+
+	@PostMapping("/UploadFile")
+	public String upload(Model model,@RequestParam("files") MultipartFile[] files) {	
+		StringBuilder fileNames = new StringBuilder();
 		for(MultipartFile file:files) {
 			Path fileNameAndPath = Paths.get(uploadDirectory,file.getOriginalFilename());
 			fileNames.append(file.getOriginalFilename());
@@ -23,7 +30,6 @@ public class FileUploadController {
 				e.printStackTrace();
 			}
 		}
-		model.addAttribute("msg", "Successfully uploaded files " + fileNames.toString());
-		*/return "updown";
+		return "updown";
 	}
 }
