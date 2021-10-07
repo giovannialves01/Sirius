140b44f90eca87a9ec921ef7974ee0dbc4b98e15
giovannialves01 <giovanni.alves01@fatec.sp.gov.br>
2021-10-07 16:21:09 -0300

    O projeto ja faz download dos arquivos da maquina host remotamente

--- Projeto/src/main/java/fatec/api/Sirius/controllers/FileDownloadController.java
+++ Projeto/src/main/java/fatec/api/Sirius/controllers/FileDownloadController.java
@@ -1,61 +1,52 @@
 package fatec.api.Sirius.controllers;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.google.common.io.Files;
 
 @Controller
 public class FileDownloadController {
 	
 	BlockControllers bc = new BlockControllers();
 	
-	public static String downloadDirectory = "/";
 	public static String Directory = "../Root/Master/";
 	
 	@GetMapping("/DownloadFile")
-	public String DownloadFile(String document, String section, String subsection, String block, String code, String remark) throws Exception {		
+	public void DownloadFile(String document, String section, String subsection, String block, String code, String remark,HttpServletRequest request, HttpServletResponse response ) throws Exception {		
+		
+		String fileName = null;
 		
 		if(subsection.equals("")){
 			Directory = Directory + document + "/" + section + "/" + block + "/" + document + "-" + section + "-" + block + "c" + code + ".pdf";
-			downloadDirectory = downloadDirectory + document + "-" + section + "-" + block + "c" + code + ".pdf";
+			fileName = document + "-" + section + "-" + block + "c" + code + ".pdf";
     	}else {
     		Directory = Directory + document + "/" + section + "/" + subsection + "/" + block + "/" + document + "-" + section + "-" + subsection + "-" + block + "c" + code + ".pdf";
-    		downloadDirectory = downloadDirectory + document + "-" + section + "-" + subsection + "-" + block + "c" + code + ".pdf";
-    	}
+    		fileName = document + "-" + section + "-" + subsection + "-" + block + "c" + code + ".pdf";
+    	}				
 		
-		
-		System.out.println(Directory);
-		System.out.println(downloadDirectory);
-		File pathDownload = bc.toFile(downloadDirectory);
 		File pathDirectory = bc.toFile(Directory);		
 		
-		try {
-			InputStream in = new FileInputStream(pathDirectory);	
-			OutputStream out = new FileOutputStream(pathDownload);
-			byte[] buffer = new byte[1024];
-			int count = 0;
-			while((count = in.read(buffer)) > 0) {
-				out.write(buffer,0,count);
-			}
-			in.close();
-			out.close();
-			
-			downloadDirectory = "/";
-			Directory = "../Root/Master/";
-			
-		} catch (Exception e) {
-			return "updown";
-		}
-		
-		downloadDirectory = "/";
+		response.setContentType("application/pdf");
+        response.addHeader("Content-Disposition", "attachment; filename="+fileName);
+        try
+        {
+            Files.copy(pathDirectory, response.getOutputStream());
+            response.getOutputStream().flush();
+        } 
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
 		Directory = "../Root/Master/";
 		
-		return "updown";
+		
 	
 	}
 }
