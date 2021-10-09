c075b4cd37d854116e998463ea4ebc5501530560
Levi Motta <64503782+levizoca@users.noreply.github.com>
2021-10-09 18:43:47 -0300

    delete pastas

--- Sirius-main/Projeto/src/main/java/fatec/api/Sirius/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package fatec.api.Sirius.repository;
-
-import fatec.api.Sirius.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository <User, Long> {
-    User findByUsername(String username);
-}
