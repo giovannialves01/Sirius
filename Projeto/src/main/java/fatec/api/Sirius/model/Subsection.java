cc3059d48cc2ef808fb5b8f9bea2339a10de4d32
giovannialves01 <giovanni.alves01@fatec.sp.gov.br>
2021-10-26 11:02:40 -0300

    o upload de arquivos armazena as informações necessarios do arquivo para o gerenciamento do codelist

---
+++ Projeto/src/main/java/fatec/api/Sirius/model/Subsection.java
@@ -0,0 +1,45 @@
+package fatec.api.Sirius.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_subsection") 
+public class Subsection {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	@Column
+	private String name;		
+	
+	@ManyToOne(cascade=CascadeType.PERSIST)
+	@JoinColumn(name="subsection_id")
+	private Section Section;
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Section getSection() {
+		return Section;
+	}
+
+	public void setSection(Section section) {
+		Section = section;
+	}
+	
+}
