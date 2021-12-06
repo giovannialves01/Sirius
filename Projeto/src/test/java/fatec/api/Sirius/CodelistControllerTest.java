a6ebaa4ad48f3e41a5aca39554fdeb44cfd6689f
Felipe Silva <ffelipe.ssilva02@gmail.com>
2021-12-06 11:05:36 -0300

    apresentar

---
+++ Projeto/src/test/java/fatec/api/Sirius/CodelistControllerTest.java
@@ -0,0 +1,28 @@
+package fatec.api.Sirius;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import fatec.api.Sirius.controllers.CodelistController;
+
+class CodelistControllerTest {
+
+	CodelistController cc = new CodelistController();
+	@Test
+	void testCodelistController() {
+		
+		System.out.println("O arquivo ABC-1234-00-00c50.docx existe? " + new File("../Root/Master/ABC-1234-00-00c50.docx").exists());
+		
+		cc.deleteFiles("../Root/Master/ABC-1234-00-00c50.docx");
+		
+		System.out.println("O arquivo ABC-1234-00-00c50.docx existe? " + new File("../Root/Master/ABC-1234-00-00c50.docx").exists());
+
+		boolean resultado = new File("../Root/Master/ABC-1234-00-00c50.docx").exists();
+		boolean esperado = false;
+		
+		Assert.assertEquals(resultado, esperado);
+	}
+
+}
---
+++ Projeto/src/test/java/fatec/api/Sirius/TestDeleteLine.java
@@ -0,0 +1,38 @@
+package fatec.api.Sirius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import fatec.api.Sirius.controllers.CodelistController;
+import fatec.api.Sirius.model.Document;
+import fatec.api.Sirius.repository.DocumentRepository;
+import org.junit.Assert;
+
+@SpringBootTest
+class TestDeleteLine {
+
+	@Autowired
+	private DocumentRepository dr;
+	@Autowired
+	private CodelistController cc;
+	
+	@Test
+	void testDeleteLine() {
+		
+		Document afterDelete = null;
+		String esperado = null;
+				
+		String id = "8";
+		Document beforeDelete = dr.findById(Integer.parseInt(id));
+		System.out.println(beforeDelete.getName());
+		cc.deleteLine(id);
+		if(dr.findById(Integer.parseInt(id)) != null) {
+			afterDelete = dr.findById(Integer.parseInt(id));
+		}
+		Assert.assertNotEquals(beforeDelete, afterDelete);
+		
+		Assert.assertEquals(esperado, afterDelete);
+	}
+
+}
